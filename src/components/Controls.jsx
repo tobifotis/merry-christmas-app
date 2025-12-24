7893a09100b562a50add0f4ae0d3e14e7e4d559b
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-12-24 11:02:14 -0600

    feat: grey out button

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,19 +3,37 @@
  onDecrement ==> function
  isDecrementDisabled ==> boolean
 */
-const Controls = ({ onIncrement, onDecrement, isDecrementDisabled, isIncrementDisabled }) => {
+const Controls = ({
+    onIncrement,
+    onDecrement,
+    isDecrementDisabled,
+    isIncrementDisabled,
+}) => {
+    const disabledStyle = {
+        opacity: 0.4,
+        cursor: 'not-allowed',
+        filter: 'grayscale(100%)',
+        boxShadow: 'none',
+        transform: 'none',
+    };
+
     return (
         <>
             <button
                 onClick={onDecrement}
                 disabled={isDecrementDisabled}
                 className="btn"
+                style={isDecrementDisabled ? disabledStyle : {}}
             >
                 ➖🎁
             </button>
-            <button onClick={onIncrement} 
-            disabled={isIncrementDisabled}
-             className="btn">
+
+            <button
+                onClick={onIncrement}
+                disabled={isIncrementDisabled}
+                className="btn"
+                style={isIncrementDisabled ? disabledStyle : {}}
+            >
                 ➕🎁
             </button>
         </>
